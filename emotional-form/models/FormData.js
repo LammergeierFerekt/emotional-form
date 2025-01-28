9347bf8bf3c1f548f545e9bbc98ae9be7a1468b6
LammergeierFerekt <furdu.nicole@gmail.com>
2025-01-28 09:48:44 +0200

    Update FormData.js
    Readded name string because it gave an error with connecting to MongoDB: MongoRuntimeError: Unable to parse <your_database_url> with URL

--- emotional-form/models/FormData.js
+++ emotional-form/models/FormData.js
@@ -3,6 +3,7 @@ const sanitizeHtml = require('sanitize-html');
 
 // Define the schema
 const formDataSchema = new mongoose.Schema({
+    name: { type: String, required: true, trim: true },
   email: { 
     type: String, 
     required: true, 
